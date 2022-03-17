fe822cbba333ba0c58048724ace0475832b90bfb
Everett Griffiths <everett@fireproofsocks.com>
2022-03-17 10:21:39 -0400

    Removes ecto dependency, adds excoveralls, bumps version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Inspecto.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
 
   @source_url "https://github.com/fireproofsocks/inspecto"
 
@@ -21,9 +21,9 @@ defmodule Inspecto.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:excoveralls, "~> 0.14.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false},
-      {:ecto, "~> 3.7.1"},
       {:ex_doc, "~> 0.28", runtime: false}
     ]
   end
@@ -33,6 +33,9 @@ defmodule Inspecto.MixProject do
     Inspecto (Inspect + Ecto) is a utility to help inspect and summarize your
     Ecto schemas, giving you an overview of the shape of your database.
     """
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   def links do
     %{
@@ -66,6 +69,7 @@ defmodule Inspecto.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       test_coverage: [
         tool: ExCoveralls
